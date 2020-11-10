dbf513a9d1124a82c60cacafdba9942dab149cad
Dior.Shelton <dior.shelton@gmail.com>
2020-11-10 16:11:05 -0600

    remove list style from input

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
   const taskbtn = document.getElementById('plus-btn').addEventListener
+
+
+  
+
   ('click',(e)=> {
-  let task = prompt('What do you need to add?');
-  let taskOne = ' Take out the trash ';
-  //capture content for list 
+  let input = document.createElement('input');
+  input.type = 'text';
+  //let task = prompt('What do you need to add?');
+  //capture content for list
+
   const newItem = document.createElement('li');
   //create li for list
-  newItem.append(task);
+
+  newItem.append(input);
   //add text to newly created li node
+
   const taskList = document.getElementById('list');
   //reference to UL
+
   let parentDiv =document.getElementById('not-sure');
   //reference to parent node
   parentDiv.insertBefore(newItem, taskList);
