3a87c6fdb400d545390a63dfcd274a1cb0159655
Dior.Shelton <dior.shelton@gmail.com>
2021-04-03 13:26:23 -0400

    Remove div surrounding form.

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,29 @@ document.addEventListener('DOMContentLoaded', function(){
   taskbtn.addEventListener('click',(e)=> {
 
   let input = document.getElementById('newtask').value;
-  //capture content for list
+  //capture value in form
 
   const newItem = document.createElement('li');
-  //create li for task list
-  
+  //create html element for to do list
+
+  let testNode = 
+
   newItem.append(input);
-  //add text to newly created li node
+  //add to do item to list
 
   const taskList = document.getElementById('list');
   //reference to UL
 
-  let parentDiv = document.getElementById('not-sure');
+  let parentDiv = document.getElementById('toDoList');
   //reference to parent node
 
+    
   parentDiv.insertBefore(newItem, taskList);
-  //insert into li
+  //insert item into list
 
   document.getElementById('newtask').value = " ";
   //clear form input after adding to list
+
 
   e.preventDefault();
   })
